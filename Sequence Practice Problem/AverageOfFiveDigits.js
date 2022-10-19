ba6ddadad81756127dee6d0471dde217eb45b195
unknown <sidhodhank2018@gmail.com>
2022-10-19 17:54:50 +0530

    Add: Average and Sum OF Five Two Digit Number

---
+++ Sequence Practice Problem/AverageOfFiveDigits.js
@@ -0,0 +1,9 @@
+let firstNumber = Math.floor(Math.random() * 100) + 10;
+let SecondNumber = Math.floor(Math.random() * 100) + 10;
+let thirdNumber = Math.floor(Math.random() * 100) + 10;
+let fourthNumber = Math.floor(Math.random() * 100) + 10;
+let fifthNumber = Math.floor(Math.random() * 100) + 10;
+
+let sum = firstNumber + SecondNumber + thirdNumber + fourthNumber + fifthNumber;
+console.log("Sum of Five Two digit Numbers=" + sum);
+console.log("Avrage of Five Two digit Numbers=" + sum / 2);
