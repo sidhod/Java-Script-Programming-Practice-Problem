28b6c8acd295643bf0fb83bfffddc79399edea24
unknown <sidhodhank2018@gmail.com>
2022-10-19 17:48:44 +0530

    Add: Random Function To Two Add Two Dice Number

---
+++ Sequence Practice Problem/AddTwoDice.js
@@ -0,0 +1,4 @@
+let firstNumbcer = Math.floor(Math.random() * 7) + 1;
+let SecondNumbcer = Math.floor(Math.random() * 7) + 1;
+let sum = firstNumbcer + SecondNumbcer;
+console.log("Sum of two dice=" + sum);
