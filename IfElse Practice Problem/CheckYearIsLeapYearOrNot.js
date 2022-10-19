d452bbf972ac3a53c7bc47473c72e12caca58f34
unknown <sidhodhank2018@gmail.com>
2022-10-19 19:54:57 +0530

    Add: Check Given Year Leap Year Or Not

---
+++ IfElse Practice Problem/CheckYearIsLeapYearOrNot.js
@@ -0,0 +1,15 @@
+
+let year = Math.floor(Math.random() * 9999) + 1000;
+if (year % 4 == 0) {
+    console.log(year + " is leap year");
+    if (year % 400 == 0) {
+        console.log(year + " is leap year");
+        if (year % 100 == 0) {
+            console.log(year + " is leap year");
+
+        }
+        else {
+            console.log(year + " is not  leap year");
+        }
+    }
+}
