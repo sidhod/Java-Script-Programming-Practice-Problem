b78758495ff6c6e0aa120887088cd6912decedfa
unknown <sidhodhank2018@gmail.com>
2022-10-19 18:07:36 +0530

    Add: Area Of Reactangular Plot In Meter

--- Sequence Practice Problem/UnitConversion.js
+++ Sequence Practice Problem/UnitConversion.js
@@ -3,4 +3,10 @@ let in1 = 12;
 let in2 = 42
 let ft2 = in2 * ft1 / in1
 console.log("42 inch=" + ft2 + "feet");
+let lengthOfPlot = 60;
+let widthOfPlot = 40;
+let areaInFeet = lengthOfPlot * widthOfPlot;
+let areaInMeter = areaInFeet * 0.3048;
+console.log("Area Of Reactangular Plot In Meter=" + areaInMeter);
+
 
