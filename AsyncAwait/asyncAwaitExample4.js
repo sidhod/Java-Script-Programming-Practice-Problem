408dd1a73ae0765f3d1f519755f64e2b07dc190f
unknown <sidhodhank2018@gmail.com>
2022-11-06 21:59:19 +0530

    Add: Async Await Example No 4

---
+++ AsyncAwait/asyncAwaitExample4.js
@@ -0,0 +1,41 @@
+// Example of Async Await
+//otp validation
+function genrateOtp() {
+    let otpgenrate = Math.floor(Math.random() * 7000) + 1000;
+    return otpgenrate;
+
+}
+// timeout for 3 sec
+function sendOtp() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(genrateOtp());
+        }, 3000);
+    });
+}
+
+function otpvalidation(otp, otpgenrate) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            if (otp == otpgenrate) {
+                resolve(genrateOtp());
+            } else {
+                console.log("Validation Fails..")
+            }
+
+        }, 3000);
+    });
+}
+
+//async fuction
+async function asyncCall() {
+    console.log("Wait....Otp is genrated");
+    let otpgenrate = await sendOtp();
+    console.log("--Otp Send To Your Mobile--");
+    const prompt = require('prompt-sync')();
+    let otp = prompt("Enter Otp You Got On Mobile: ");
+    console.log("Wait For Otp Validation");
+    await otpvalidation(otp, otpgenrate);
+}
+//Call Function
+asyncCall();
