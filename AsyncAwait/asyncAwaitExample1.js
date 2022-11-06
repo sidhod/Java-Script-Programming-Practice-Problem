26375c80bef273e8d187a3df4969c4a57307fa54
unknown <sidhodhank2018@gmail.com>
2022-11-06 20:18:59 +0530

    Add: Async Await Example No 1

---
+++ AsyncAwait/asyncAwaitExample1.js
@@ -0,0 +1,26 @@
+// Example of Async Await
+//add fuction
+function add(a, b) {
+    return a + b;
+}
+// timeout for 3 sec
+function resolveAfter3seconds() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(add(5, 5));
+        }, 1000);
+    });
+}
+
+//async fuction
+async function asyncCall() {
+    let add = 5;
+    console.log("calling");
+    const result = await resolveAfter3seconds();
+    console.log("after resolve result ", result);
+    const FinalResult = result + add;
+    console.log("after resolve ", FinalResult);
+
+}
+//Call Function
+asyncCall();
